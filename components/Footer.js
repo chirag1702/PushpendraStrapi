f02d91ddf0cd699811123ace0b6c4dbf03b91d5c
chirag1702 <chirag.jangid1702@gmail.com>
2021-11-17 14:31:47 +0530

    .

--- components/Footer.js
+++ components/Footer.js
@@ -3,7 +3,7 @@ import styles from "@/styles/Footer.module.css";
 export default function Footer() {
   return (
     <footer className={styles.footer}>
-      <p>Copyright &copy; Sport News 2021</p>
+      {/* <p>Copyright &copy; Sport News 2021</p> */}
     </footer>
   );
 }
